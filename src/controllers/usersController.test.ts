a6109c8d70d4ffdee7d90e1a7aa75f5f92fb91fd
user.email like : "eaGraciliana@Gmail.com <user.email like : "eaGraciliana@Gmail.com>
2023-05-27 18:42:24 -0300

    DNJ-5 :sparkles: feat: Mocks e testes unitarios

--- src/controllers/usersController.test.ts
+++ src/controllers/usersController.test.ts
@@ -1,8 +1,38 @@
-describe('Users Controller', () => {
-  it('Deve somar 1+1', () => { 
-    function soma(a: number, b: number){
-      return a+b 
-    }
-    const resultado = soma(1, 2)
-  expect(resultado).toBe(3)})
-})
+import { Request } from "express";
+import { makeMockResponse } from "../mocks/mockResponse";
+import { UsersController } from "./usersController";
+
+describe("Users Controller", () => {
+	const usersController = new UsersController();
+	const mockRequest = {} as Request;
+	const mockResponse = makeMockResponse();
+	it("Deve listar os nossos usuários", () => {
+		usersController.listarUsuario(mockRequest, mockResponse);
+		expect(mockResponse.state.status).toBe(200);
+		expect(mockResponse.state.json).toHaveLength(4);
+	});
+
+	it("Deve criar um novo usuário", () => {
+		mockRequest.body = {
+			name: "Novo usuário",
+		};
+
+		usersController.criarUsuario(mockRequest, mockResponse);
+		expect(mockResponse.state.status).toBe(201);
+		expect(mockResponse.state.json).toMatchObject({
+			mensagem: `Usuário Novo usuário criado`,
+		});
+	});
+
+	it("Não deve criar um usuário com o nome em branco", () => {
+		mockRequest.body = {
+			name: "",
+		};
+
+		usersController.criarUsuario(mockRequest, mockResponse);
+		expect(mockResponse.state.status).toBe(403);
+		expect(mockResponse.state.json).toMatchObject({
+			mensagem: "Não é possível criar usuários sem um nome",
+		});
+	});
+});
